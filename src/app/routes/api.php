9dbda11ef0e9208f809d3df828497259e00a70bf
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-12-11 22:57:46 +0900

    SPA認証機能を追加

--- src/app/routes/api.php
+++ src/app/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterApiController;
+use App\Http\Controllers\Auth\LoginApiController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// ログインしたユーザーのみアクセスできる
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/hoge', function(){
+        return 'auth is working';
+    });
+});
+
+// 認証
+Route::get('/reset', [HomeController::class, 'reset']);
+Route::post('/register', [RegisterApiController::class, 'register']);
+Route::post('/login', [LoginApiController::class, 'login']);
+Route::post('/logout', [LoginApiController::class, 'logout']);
